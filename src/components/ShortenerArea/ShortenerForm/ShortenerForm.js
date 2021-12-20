bbc7b6333e1aee5f85e6c34aef328d7aca31aec9
Felipe Pacelli <fgpacelli1@gmail.com>
2021-12-20 18:07:10 -0300

    added state to Shortener form

--- src/components/ShortenerArea/ShortenerForm/ShortenerForm.js
+++ src/components/ShortenerArea/ShortenerForm/ShortenerForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import Button from "../../UI/Button/Button";
 import Input from "../../UI/Input/Input";
 import Card from "../../UI/Card/Card";
@@ -6,11 +6,23 @@ import Card from "../../UI/Card/Card";
 import styles from "./ShortenerForm.module.css";
 
 function ShortenerForm() {
+    const inputRef = useRef(null);
+    const [error, setError] = useState(false);
+
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        const exp = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%.\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%\+.~#?&\\=]*)/gm);
+    };
+
+    console.log(error);
+
+
+
     return (
         <Card className={styles['shortener-form_card']}>
-            <form className={styles['shortener-form']} id="shortenerForm" noValidate autoComplete="off">
+            <form onSubmit={formSubmitHandler} className={styles['shortener-form']} id="shortenerForm" noValidate autoComplete="off">
                 <fieldset className={styles['shortener-form_form-controls']} name="Form Controls">
-                    <Input type="text" ariaLabel="Add a link here" placeholder="Shorten a Link here..." />
+                    <Input inputref={inputRef} type="text" ariaLabel="Add a link here" placeholder="Shorten a Link here..." />
                     <small style={{display: 'none'}} title="Error Message">Please add a valid link...</small>
                 </fieldset>
                 <fieldset className={styles['shortener-form_form-actions']} name="Form Actions">
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -5,6 +5,7 @@ function Input(props) {
     return (
         <input
             className={`${styles['input']} ${props.className}`}
+            ref={props.inputref}
             type={props.type}
             id={props.id}
             aria-label={props.ariaLabel}
