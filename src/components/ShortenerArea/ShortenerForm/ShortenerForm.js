7847e58162b0a249c0ee4331788fd0150dbb1b4c
Felipe Pacelli <fgpacelli1@gmail.com>
2021-12-20 15:39:58 -0300

    added Shortener Form

--- src/components/ShortenerArea/ShortenerForm/ShortenerForm.js
+++ src/components/ShortenerArea/ShortenerForm/ShortenerForm.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import axios from "axios";
 import Button from "../../UI/Button/Button";
+import Input from "../../UI/Input/Input";
+import Card from "../../UI/Card/Card";
+
+import styles from "./ShortenerForm.module.css";
 
 
 //validate input value;
@@ -8,18 +12,30 @@ import Button from "../../UI/Button/Button";
 //pass the object with the response to Shortener Area in a prop function;
 
 function ShortenerForm() {
+    const inputRef = useRef(null);
+
+    //set state to make http request;
+    const formSubmitHandler = event => {
+        event.preventDefault();
+        console.log(`Submitted: ${inputRef.current.value}`);
+
+        inputRef.current.value = null;
+    };
+
     return (
-        <form>
-            <fieldset>
-                <label htmlFor="linkToShort">
-                    <span>Please enter a valid link...</span>
-                </label>
-                <input id="linkToShort" placeholder="Add a link here..."></input>
-            </fieldset>
-            <fieldset>
-                <Button>Shorten It!</Button>
-            </fieldset>
-        </form>
+        <Card className={styles['shortener-form_card']}>
+            <form className={styles['shortener-form_container']} id="shortenLinkForm" autoComplete="off" noValidate onSubmit={formSubmitHandler}>
+                <fieldset className={styles['shortener-form_controls']} name="Form Controls">
+                    <label style={{display: 'block'}} htmlFor="linkToShort">
+                        {/* <span name="error message">Please enter a valid link...</span> */}
+                    </label>
+                    <Input inputref={inputRef} id="linkToShort" type="text" placeholder="Shorten a link here..." />
+                </fieldset>
+                <fieldset className={styles['shortener-form_actions']} name="Form Actions">
+                    <Button type="submit" isPrimary={false}>Shorten It!</Button>
+                </fieldset>
+            </form>
+        </Card>
     );
 };
 
---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Input.module.css";
+
+function Input(props) {    
+    return (
+        <input
+            className={`${styles['input']} ${props.className}`}
+            ref={props.inputref}
+            type={props.type}
+            id={props.id}
+            aria-label={props.ariaLabel}
+            placeholder={props.placeholder}
+            onClick={props.onClick}
+            onChange={props.onChange}>
+            {props.children}
+        </input>
+    );
+};
+
+export default Input;
