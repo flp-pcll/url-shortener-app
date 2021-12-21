8bd60af7deba49b0d9779fcd7c2c75b4dab566aa
Felipe Pacelli <fgpacelli1@gmail.com>
2021-12-21 16:38:38 -0300

    added style to shortened links cards

--- src/components/ShortenerArea/ShortenedLink/ShortenedLink.js
+++ src/components/ShortenerArea/ShortenedLink/ShortenedLink.js
@@ -8,13 +8,13 @@ function ShortenedLink(props) {
         <li className={styles['shortened-link_container']} id={props.id}>
             <Card className={styles['shortened-link_container--card']}>
                 <section className={styles['shortened-link_links-area']} name="Links Area">
-                    <span style={{ display: 'inline' }}>
-                        <p>
+                    <span className={styles['original-link_container']}>
+                        <p className={styles['shortened-link__originalURL']}>
                             {props.originalURL.length > 40 && `${props.originalURL.substr(0, 40)}...`}
                             {props.originalURL.length < 40 && `${props.originalURL}`}
                         </p>
                     </span>
-                    <span className={styles['shortened-link-area']}><p href={props.shortenedURL}>{props.shortenedURL}</p></span>
+                    <span className={styles['shortened-link-area']}><p>{props.shortenedURL}</p></span>
                 </section>
                 <div className={styles['shortened-link_controls-area']} name="Controls Area">
                     <Button>Copy</Button>
